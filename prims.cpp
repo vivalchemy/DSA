c34ea4b9602646fe2266d9ad06a923eb7655ad82
vivalchemy <vivalchemy.social+github@gmail.com>
2024-03-02 18:34:23 +0530

    finished prims algorithm

--- prims.cpp
+++ prims.cpp
@@ -15,7 +15,7 @@ struct Node {
   char data; // stores the data of the Node
   std::unordered_map<Node *, int>
       neighbours; // stores the nodes and their
-                  // corresponding distance from the node
+                  // corresponding weight from the node
 };
 
 class Graph {
@@ -23,10 +23,11 @@ private:
   std::unordered_map<char, Node *> nodes; // list of nodes
   // used to give unique identification to the Node
   std::unordered_map<Node *, std::pair<int, Node *>>
-      path;                             // final dist | pred array
-  std::unordered_set<Node *> unvisited; // node | distance | predecessor
+      path;                             // final weight | pred array
+  std::unordered_set<Node *> unvisited; // node | weight | predecessor
   std::vector<Node *> visited;
   Node *source = nullptr;
+  int cost = 0;
 
   void _clearPaths() {
     path.clear();
@@ -37,11 +38,18 @@ private:
     }
   }
 
-  int _getDistance(const char &dest) {
+  void _getCost() {
     if (path.empty()) { // Check if shortest paths have been calculated
-      return -1;
+      std::cerr
+          << CError
+          << "Error: Shortest paths have not been calculated. Run prims's "
+             "algorithm first."
+          << ColorOff << std::endl;
+      return;
     }
-    return path[nodes[dest]].first;
+    for (const std::pair<Node *const, std::pair<int, Node *>> &node : path) {
+      cost += node.second.first;
+    }
   }
 
 public:
@@ -68,16 +76,16 @@ public:
     }
   }
 
-  void addEdge(const char &from, const char &to, int distance) {
+  void addEdge(const char &from, const char &to, int weight) {
     if (nodes.count(from) && nodes.count(to)) {
-      nodes[from]->neighbours[nodes[to]] = distance;
+      nodes[from]->neighbours[nodes[to]] = weight;
     } else {
       std::cerr << CError << "Error: Node(s) not found in the graph."
                 << ColorOff << std::endl;
     }
   }
 
-  void dijkstra() {
+  void prims() {
     if (source == nullptr) {
       std::cerr << CError << "Error: Source node not set." << ColorOff
                 << std::endl;
@@ -100,18 +108,18 @@ public:
     _clearPaths();
     source->neighbours[source] = 0;
     for (const std::pair<const char, Node *> &node : nodes) {
-      path[node.second] = {INT_MAX, nullptr}; // Initialize distance to infinity
+      path[node.second] = {INT_MAX, nullptr}; // Initialize weight to infinity
                                               // and predecessor to nullptr
     }
-    path[source] = {0, source}; // Distance from source to itself is 0
+    path[source] = {0, source}; // Weight from source to itself is 0
 
     while (!unvisited.empty()) {
-      // minimum distance in the corresponding neighbours
-      int minDistance = INT_MAX;
+      // minimum weight in the corresponding neighbours
+      int minWeight = INT_MAX;
       Node *minNode = nullptr;
       for (Node *const &node : unvisited) {
-        if (path.count(node) > 0 && path[node].first < minDistance) {
-          minDistance = path[node].first;
+        if (path.count(node) > 0 && path[node].first < minWeight) {
+          minWeight = path[node].first;
           minNode = node;
         }
       }
@@ -124,60 +132,43 @@ public:
       unvisited.erase(minNode);
       visited.push_back(minNode);
 
-      for (const auto &[node, distance] : minNode->neighbours) {
-        // if the node exist in the path
-        int newDistance = path[minNode].first + distance;
-        if (path[node].first > newDistance) {
-          // if the new distance is lesser than the old distance the update it
+      for (const auto &[node, weight] : minNode->neighbours) {
+        if (path[node].first > weight) {
+          // if the new weight is lesser than the old weight then update it
           // in the path array
-          path[node].second = minNode;    // update the predecessor node
-          path[node].first = newDistance; // update the distance
+          path[node].second = minNode; // update the predecessor node
+          path[node].first = weight;   // update the weight
         }
       }
     }
   }
 
+  // print the node | weight | pred map
   void printPath() {
     if (path.empty()) { // Check if shortest paths have been calculated
       std::cerr
           << CError
-          << "Error: Shortest paths have not been calculated. Run Dijkstra's "
+          << "Error: Shortest paths have not been calculated. Run prims's "
              "algorithm first."
           << ColorOff << std::endl;
       return;
     }
     std::cout << "Source Node :\t" << source->data << std::endl;
-    std::cout << "|Node\t|Distance\t|Predecessor\t|" << std::endl;
-    std::cout << "|----\t|--------\t|-----------\t|\n";
+    std::cout << "|Node\t|Weight\t|Predecessor\t|" << std::endl;
+    std::cout << "|----\t|------\t|-----------\t|\n";
     for (const std::pair<Node *const, std::pair<int, Node *>> &node : path) {
-      int dist = node.second.first == INT_MAX ? -1 : node.second.first;
-      std::cout << "|" << node.first->data << "\t|" << dist << "\t\t|"
+      int weight = node.second.first == INT_MAX ? -1 : node.second.first;
+      std::cout << "|" << node.first->data << "\t|" << weight << "\t|"
                 << node.second.second->data << "\t\t|" << std::endl;
     }
-  }
-  // prints the route from the source to the destination
-  void printRoute(const char &dest) {
-    if (path.empty()) { // Check if shortest paths have been calculated
-      std::cerr
-          << CError
-          << "Error: Shortest paths have not been calculated. Run Dijkstra's "
-             "algorithm first."
-          << ColorOff << std::endl;
-      return;
-    }
-    if (dest != source->data) {
-      printRoute(path[nodes[dest]].second->data);
-    } else {
-      // when you reach the source
-      std::cout << "The route is:\t" << dest;
-      return;
-    }
-    std::cout << " -> " << dest;
+
+    printCost();
   }
 
-  void printDistance(const char &dest) {
-    std::cout << "\nThe distance of " << dest << " from " << source->data
-              << " is " << _getDistance(dest) << std::endl;
+  // print to total cost of the spanning tree
+  void printCost() {
+    _getCost();
+    std::cout << "\nThe cost of spanning tree is " << cost << std::endl;
   }
   // Print the graph representation
   void printGraph() {
@@ -201,17 +192,17 @@ int main() {
   Graph graph;
   int op = 1;
   char source, dest;
-  int dist;
+  int weight;
   char vertex;
   while (op) {
     std::cout << "\n--------------------------------------------------------\n";
-    std::cout
-        << "Enter the option from the menu:\n\n1.Add a vertex\t2.Add an "
-           "edge\t3.Set source node\t4.Run dijstra algorithm of the given "
-           "data\n5.Print graph\t6.Print the "
-           "shortest distance\t7.Find route to the destination\t8.Clear "
-           "graph\n0.Exit"
-        << std::endl;
+    std::cout << "Enter the option from the menu:\n\n1.Add a vertex\t2.Add an "
+                 "edge\t3.Set source node\t4.Run Prims algorithm on the given "
+                 "data\n5.Print graph\t6.Print the "
+                 "weight-predecessor array\t7.Print the cost of spannig "
+                 "tree\t8.Clear "
+                 "graph\n0.Exit"
+              << std::endl;
     std::cout << "Option :\t";
     std::cin >> op;
     switch (op) {
@@ -220,37 +211,34 @@ int main() {
       std::cin >> vertex;
       graph.addNode(vertex);
       break;
-    case 2: // Add the vertex
+    case 2: // Add the edge
       std::cout << "Enter the source :\t";
       std::cin >> source;
       std::cout << "Enter the destination :\t";
       std::cin >> dest;
-      std::cout << "Enter the distance :\t";
-      std::cin >> dist;
-      graph.addEdge(source, dest, dist);
+      std::cout << "Enter the weight :\t";
+      std::cin >> weight;
+      graph.addEdge(source, dest, weight);
       break;
-    case 3:
+    case 3: // Set the source node
       std::cout << "Enter the source node :\t";
       std::cin >> source;
       graph.setSource(source);
       std::cout << "The source is " << graph.getSource()->data << std::endl;
       break;
-    case 4:
-      graph.dijkstra();
+    case 4: // apply prims
+      graph.prims();
       break;
-    case 5:
+    case 5: // print the graph
       graph.printGraph();
       break;
-    case 6:
+    case 6: // print the weight | pred map
       graph.printPath();
       break;
-    case 7:
-      std::cout << "Enter the destination :\t";
-      std::cin >> dest;
-      graph.printRoute(dest);
-      graph.printDistance(dest);
+    case 7: // print the total cost of spannig tree
+      graph.printCost();
       break;
-    case 8:
+    case 8: // clear the data in graph
       graph.clearGraph();
       break;
     }
